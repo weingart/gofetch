2580bb44dbf7470e37e29e1ec61af3ce6a87dfe0
Tobias Weingartner <weingart@tepid.org>
2022-10-10 06:09:39 +0000

    Upate to using golang.org/dl

--- go.mod
+++ go.mod
@@ -1,15 +1,5 @@
 module github.com/weingart/gofetch
 
-require (
-	cloud.google.com/go v0.34.0
-	github.com/blang/semver v3.5.1+incompatible
-	github.com/google/martian v2.1.0+incompatible // indirect
-	github.com/googleapis/gax-go v2.0.2+incompatible // indirect
-	github.com/kr/pretty v0.1.0
-	go.opencensus.io v0.18.0 // indirect
-	golang.org/x/net v0.0.0-20181220203305-927f97764cc3
-	golang.org/x/oauth2 v0.0.0-20181203162652-d668ce993890 // indirect
-	google.golang.org/api v0.0.0-20181221000618-65a46cafb132 // indirect
-	google.golang.org/genproto v0.0.0-20181221175505-bd9b4fb69e2f // indirect
-	google.golang.org/grpc v1.17.0 // indirect
-)
+go 1.19
+
+require github.com/blang/semver v3.5.1+incompatible
--- listdir.go
+++ listdir.go
@@ -1,184 +1,85 @@
 package main
 
 import (
+	"encoding/json"
+	"flag"
 	"fmt"
+	"io/ioutil"
 	"log"
-	"regexp"
+	"net/http"
 	"sort"
 	"strings"
-	"unicode"
-	"unicode/utf8"
 
-	"cloud.google.com/go/storage"
 	"github.com/blang/semver"
-	"github.com/kr/pretty"
-	"golang.org/x/net/context"
 )
 
+const goURL = `https://go.dev/dl/?mode=json` // List of latest production versions
+
+var verbose = flag.Bool("v", false, "Print verbose instructions.")
+
 type GoPackage struct {
-	Filename string
-	Version  semver.Version
+	Version string
+	Stable  bool
+	v       semver.Version
 }
 
 type GoPackages []*GoPackage
 
 func (g GoPackages) Len() int           { return len(g) }
-func (g GoPackages) Less(i, j int) bool { return g[i].Version.LT(g[j].Version) }
+func (g GoPackages) Less(i, j int) bool { return g[i].v.LT(g[j].v) }
 func (g GoPackages) Swap(i, j int)      { g[i], g[j] = g[j], g[i] }
 
-func findMetadata(parts []string) int {
-
-	i := 1
-	for ; i < len(parts); i++ {
-		r, _ := utf8.DecodeRuneInString(parts[i])
-		if !unicode.IsNumber(r) {
-			return i
-		}
-	}
-
-	return i
-}
-
-func parseMetadata(b, m string) string {
-
-	return "+" + b + "." + strings.Replace(m, "osx10.", "osx10-", -1)
-}
-
-func parseExtra(s string) string {
-	re1 := regexp.MustCompile(`([[:digit:]]+)`)
-	re2 := regexp.MustCompile(`\.\.+`)
-
-	s = strings.Replace(s, "-", ".", -1)
-	s = re1.ReplaceAllString(s, ".$1.")
-	s = re2.ReplaceAllString(s, ".")
-	s = strings.TrimLeft(s, ".")
-	s = strings.TrimRight(s, ".")
-	if s != "" {
-		s = "-" + s
-	}
-
-	return s
-}
-
-func parseVersion(s string) (string, string) {
-	v := regexp.MustCompile(`^[[:digit:]]+(\.[[:digit:]]+)+`).FindString(s)
-
-	return v, strings.TrimPrefix(s, v)
-}
-
-func parseVersionAndExtra(s string) (string, string) {
-	v, e := parseVersion(s)
-	for len(strings.Split(v, ".")) < 3 {
-		v = v + ".0"
-	}
-
-	return v, parseExtra(e)
-}
-
-func TrimBase(s string) (string, string) {
-	base := regexp.MustCompile(`^[[:alpha:]-]+`).FindString(s)
-
-	return strings.TrimPrefix(s, base), strings.TrimRight(base, "-")
-}
-
-func TrimMetadata(s string) (string, string) {
-	part := strings.Split(s, ".")
-	metaidx := findMetadata(part)
-	metadata := strings.Join(part[metaidx:], ".")
-
-	return strings.TrimSuffix(s, "."+metadata), metadata
-}
-
-// FilenameToSemVer tries to convert a Go package name to something that
-// can be parsed into something resembling a SemVer along with some extra
-// information to identify the full filename.
+// GoVersionToSemVer tries to convert a Go version to something that can be
+// parsed into something resembling a SemVer.
 //
 // Dear $DEITY, why was this not done before!?!
 //
-// $AUTHOR proposes that future filenames of published Go packages follow
-// a much more parseable and simple format:
-//
-// $FILENAME-$SEMVER
+// $AUTHOR proposes that future versions of Go follow a more parseable and
+// simple format: $THING-$SEMVER.EXTENSIONS
 //
 // For example:
 //
-// go-1.8-beta.1+linux-s390x.tar.gz
-// go-tools-1.8.1+src.tar.gz
-//
-// In this way, splitting the first part off is simple, as long as the
-// first portion does not include the regex "-[0-9]".
-//
-func FilenameToSemVer(name string) (*GoPackage, error) {
+//	go-1.8-beta.1+linux-s390x.tar.gz
+//	go-tools-1.8.1+src.tar.gz
+func GoVersionToSemVer(name string) (semver.Version, error) {
 	// Because we have something even more... ugh, don't even.
-	//n := strings.Replace(name, "-bootstrap-", ".bootstrap-", -1)
-	n := name
-	fmt.Println("Filename:", name)
-
-	// Grab the base
-	n, base := TrimBase(n)
-
-	// Grab suffix/build info
-	n, metadata := TrimMetadata(n)
-	metadata = parseMetadata(base, metadata)
-
-	// Find the first non-extension part
-	version, rest := parseVersionAndExtra(n)
-
-	// Should look like a semver now (maybe)
-	fmt.Println("Parsing:", version+rest+metadata)
-	v, err := semver.ParseTolerant(version + rest + metadata)
-	if err != nil {
-		return nil, err
-	}
-	return &GoPackage{
-		Filename: name,
-		Version:  v,
-	}, nil
-}
-
-func GetFilenames(c *storage.Client, bucket string) (names []string) {
-
-	b := c.Bucket(bucket)
-	q := &storage.Query{
-		Delimiter: "/",
-		// Prefix: "go1.7.4",
-		// Versions: false,
-	}
-
-	it := b.Objects(context.Background(), q)
-	for obj, err := it.Next(); obj != nil; obj, err = it.Next() {
-		if err != nil {
-			log.Fatal(err)
-		}
-		if len(obj.Name) > 0 {
-			names = append(names, obj.Name)
-		}
-	}
-
-	return
+	return semver.ParseTolerant(strings.TrimPrefix(name, "go"))
 }
 
 func main() {
-	client, err := storage.NewClient(context.Background())
+	flag.Parse()
+
+	resp, err := http.Get(goURL)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("Error retrieving Go versions: %v", err)
+	}
+	buf, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalf("Error reading response: %v", err)
 	}
 
-	files := GetFilenames(client, "golang")
+	var versions GoPackages
+	if err := json.Unmarshal(buf, &versions); err != nil {
+		log.Fatalf("Error parsing Go versions: %v", err)
+	}
 
-	versions := make(GoPackages, 0)
-	for _, name := range files {
-		gp, err := FilenameToSemVer(name)
+	for _, pkg := range versions {
+		pkg.v, err = GoVersionToSemVer(pkg.Version)
 		if err != nil {
-			log.Printf("Bad parse of filename '%v': %v\n", name, err)
-			continue
+			log.Fatalf("Error decoding Go version %q: %v", pkg.Version, err)
 		}
-		versions = append(versions, gp)
 	}
 
-	sort.Sort(versions)
-	for _, v := range versions {
-		pretty.Println(v)
-		fmt.Println("")
+	sort.Sort(sort.Reverse(versions))
+	if len(versions) <= 0 {
+		log.Fatal("No Go versions detected!")
+	}
+	if *verbose {
+		fmt.Println("# Run the following commands:")
+		fmt.Printf("go install golang.org/dl/%s@latest\n", versions[0].Version)
+		fmt.Printf("%s download\n", versions[0].Version)
+		fmt.Printf("ln -sf $(which %s) $(which go)\n", versions[0].Version)
+	} else {
+		fmt.Println(versions[0].Version)
 	}
 }
